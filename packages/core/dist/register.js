6d38f4b03b0174626a6dffd9d0bf4ba84fb415f1
Patrick Hadson <pkhadson@gmail.com>
2023-09-30 20:40:55 -0300

    removing dist

--- packages/core/dist/register.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const register = () => {
-    console.log("oioioi");
-    return "sdsd";
-};
-exports.default = register;
